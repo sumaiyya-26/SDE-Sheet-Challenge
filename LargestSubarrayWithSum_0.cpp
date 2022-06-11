92975937c50efe38f46b3378fe57236b2f16086a
Sumaiyya Anwar <anwarsharmeen@gmail.com>
2022-06-11 23:20:34 +0530

    syntax error

--- LargestSubarrayWithSum_0.cpp
+++ LargestSubarrayWithSum_0.cpp
@@ -50,6 +50,6 @@
     }
     int main(){
         int n;cin>>n;
-        vector<int>v;for(int i=0;i<n;i++){int x;xin>>x;v.push_back(x);}
+        vector<int>v;for(int i=0;i<n;i++){int x;cin>>x;v.push_back(x);}
         cout<<maxLen(v,n);
     }
